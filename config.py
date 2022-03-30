438a8907d00ca64ba52fd1e6da695cdde68636b5
Ronak Sharma <rsharma29230@gmail.com>
2022-03-30 07:25:20 +0100

    Add marshmallow and update name

--- config.py
+++ config.py
@@ -12,14 +12,14 @@ database = dict(
 # ------------------DATABASE QUERIES-------------#
 
 database_query = dict(
-    add_person = '''  INSERT INTO library_practice.Person (Full_Name, Age, Weight, Date_Of_Birth, Country_Of_Birth, Email, Reading_Budget, Gender, Married)
+    add_user = '''  INSERT INTO library_practice.Person (Full_Name, Age, Weight, Date_Of_Birth, Country_Of_Birth, Email, Reading_Budget, Gender, Married)
                     VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)
                 ''',
     add_book = '''  INSERT INTO library_practice.Book (Book_Name, Author_Name, Genre, Price, Current_Owner, Date_of_Issue)
                     VALUES (%s,%s,%s,%s,%s,%s)
                 ''',
 
-    display_person = '''
+    display_user = '''
                     SELECT * FROM library_practice.Person;
                     ''',
 
