fcac3accb99815bb95894dc8ad2242b199bd195c
Ronak Sharma <34181525+RonakSharma1@users.noreply.github.com>
2022-04-08 07:16:06 +0100

    Add book (#3)
    * Add marshmallow and update name
    
    * Add model objects using dataclass
    
    * Add new user to database on POST request
    
    * Add field validation and error handling
    
    * - Add validation
    - Update scehma types
    
    * Add book schema
    
    * Add database methods for addition
    
    * open endpoint for adding new books

--- controller/library_router.py
+++ controller/library_router.py
@@ -1,5 +1,6 @@
-from xml.dom import ValidationErr
+from marshmallow import ValidationError
 from flask import Flask, request, jsonify
+from model.book import BookSchema
 from model.user import UserSchema
 from service import library_business
 
@@ -10,18 +11,16 @@ def get_user () :
     if request.method == 'GET':
         list_of_people = library_business.get_all_user()
         return list_of_people.to_html(header="true", table_id="table")
-
     elif request.method == 'POST':
         try:
             # Deserialisation
             user = UserSchema().load(request.get_json())
             library_business.add_user(user)
-            # Serialisation
+            # Serialisation (AN ID SHOULD BE RETURNED)
             schema = UserSchema(only=("full_name", "birth_date"))
             return schema.dump(user)
-        except ValidationErr as err:
-            print(err.messages)
-
+        except ValidationError as err:
+            return(err.messages)
     else:
         return jsonify(
             isError= True,
@@ -29,7 +28,26 @@ def get_user () :
             statusCode= 400,
             data= "Invalid request type"), 400
 
-@app.route("/all_book")
+@app.route("/all_book", methods=['GET', 'POST'])
 def get_book () :
-    database_book = library_business.get_all_book()
-    return database_book.to_html(header="true", table_id="table")
+    if request.method == 'GET':
+        database_book = library_business.get_all_book()
+        return database_book.to_html(header="true", table_id="table")
+    elif request.method == 'POST':
+        try:
+            # Deserialisation
+                # Use  BookSchema(many=True) when multiple objects in POST
+            book = BookSchema().load(request.get_json())
+            library_business.add_book(book)
+
+            # Serialisation (An ID should be returned)
+            schema = BookSchema(only={"book_name","author_name","price"})
+            return schema.dump(book)
+        except ValidationError as err:
+            return(err.messages)
+    else:
+        return jsonify(
+            isError= True,
+            message= "Failure",
+            statusCode= 400,
+            data= "Invalid request type"), 400
--- model/book.py
+++ model/book.py
@@ -1,16 +1,19 @@
-from dataclasses import dataclass
-from datetime import date
+from dataclasses import dataclass, field
 from marshmallow import validate
+from datetime import date
 import marshmallow_dataclass
 
 @dataclass
 class Book:
-    book_name: str
-    author_name: str
-    price: int
-    date_of_issue: date
-    genre: str
-    reading_budget: int
-    current_owner: str
-
+    book_name: str = field(metadata=dict(required=True, 
+                                         validate=validate.Length(min=1)))
+    author_name: str = field(metadata=dict(required=True, 
+                                           validate=validate.Length(min=1)))
+    genre: str = field(metadata=dict(required = False, 
+                                     load_default="Unknown",
+                                     dump_default="Unknown"))
+    price: float = field(metadata=dict(required=True))    
+    current_owner: int = field(metadata=dict(required = True))
+    date_of_issue: date = field(metadata=dict(required=True))
+    
 BookSchema = marshmallow_dataclass.class_schema(Book)
